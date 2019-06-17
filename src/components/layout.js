385598c54f170f249d4743afd3005a922920f051
Omar Tan <omartan90@hotmail.com>
2019-06-17 15:33:20 +0800

    change inline to external stylesheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import layoutStyles from './layout.module.css'
 
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
 export default ({children}) => (
-  <div style={{margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`}}>
-    <header style={{marginBottom: `1.5rem`}}>
-      <Link to='/' style={{textShadow: `none`, backgroundImage: `none`}}>
-        <h3 style={{display: `inline`}}>My Sweet Site</h3>
+  <div className={layoutStyles.container}>
+    <header>
+      <Link className={layoutStyles.h3style}>
+        <h3>My Sweet Site</h3>
       </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
+      <ul>
         <ListLink to='/'>Home</ListLink>
         <ListLink to='/about/'>About</ListLink>
         <ListLink to='/contact'>Contact</ListLink>
